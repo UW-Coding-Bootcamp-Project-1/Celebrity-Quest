573611da479566861b91d5cc162c05ece52e6c50
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-04-13 21:37:36 -0500

    WIP to put videos on HTML

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -2,7 +2,18 @@ $(document).ready(function() {
 
 // Storing YouTube API key & URL
 function videos() {
-  var youtubeURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q="  + actor + "&key=AIzaSyBvDT6JrY1UC0jumUEXSLLw-97HjDh5nb4";
+  // var youtubeURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q="  + actor + "&limit=3&key=AIzaSyBvDT6JrY1UC0jumUEXSLLw-97HjDh5nb4";
+
+  // My API Key - AIzaSyBvDT6JrY1UC0jumUEXSLLw-97HjDh5nb4
+
+  //Test
+  // var youtubeURL = "https://www.googleapis.com/youtube/v3/search?q=something&key=YOUR_API_KEY&fields=items(id,snippet(title,thumbnails(default))&part=snippet"
+
+  var youtubeURL = "https://www.googleapis.com/youtube/v3/search?q=something&key=AIzaSyC5T7Hpx41SCvhtbhlbfNIuSDq97B-TrhM&fields=items" + vidId + vidTitle + vidThumb + "&part=snippet"
+
+  // Ashley's API - AIzaSyC5T7Hpx41SCvhtbhlbfNIuSDq97B-TrhM
+
+
 
   // Creating AJAX Call
   $.ajax({
@@ -18,31 +29,33 @@ function videos() {
 
       // Gathering video info
       var vidDiv = $("#youtube")
-      var vidView = response.items[i].snippet.thumbnails.default.url;
-      var vidHeight = response.items[i].snippet.thumbnails.default.height;
-      var vidWidth = response.items[i].snippet.thumbnails.default.width;
+      var vidThumb = response.items[i].snippet.thumbnails.default;
       var vidTitle = response.items[i].snippet.title;
+      var vidId = response.items[i].id.videoId;
       var showVid = $("<video>");
       var showTitle = $("<p>").text(vidTitle);
 
       showVid.attr("src", vidView);
       showTitle.attr("title", vidTitle);
 
-      console.log("Video:", vidView, vidHeight, vidWidth);
+      
       console.log("Title:", vidTitle);
+      console.log("Video:", vidThumb);
+
+      // Try incorporating the video to play on the page instead of thumbnail
 
       vidDiv.append(showTitle);
       vidDiv.append(showVid);
     }
     
     
-        function userInput() {
-          var addInput = $("#search").val().trim();
-          celebrity.push(addInput);
-          console.log("Celebrity:", celebrity)
-        }
+        // function userInput() {
+        //   var addInput = $("#search").val().trim();
+        //   celebrity.push(addInput);
+        //   console.log("Celebrity:", celebrity)
+        // }
 
-        userInput();
+        // userInput();
 
     });
     
