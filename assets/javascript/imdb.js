fd9abca4fa3556fb667db46a5ddaff0c6ea72af2
Tashona Smith <tashona.smith@gmail.com>
2020-04-13 21:05:48 -0500

    Uncomment news api key

--- assets/javascript/imdb.js
+++ assets/javascript/imdb.js
@@ -1,7 +1,7 @@
 var celebrity = $("#search").val().trim();
 console.log("celebrity: ", celebrity);
 
-//var queryUrl = "https://newsapi.org/v2/everything?q=" + celebrity + "&apiKey=f9a29869e5cd42f4b0d2a6a4b9a85c6c";
+var queryUrl = "https://newsapi.org/v2/everything?q=" + celebrity + "&apiKey=f9a29869e5cd42f4b0d2a6a4b9a85c6c";
 
 
 //Add to on-click function: $("#actor-name").text(celebrity);
